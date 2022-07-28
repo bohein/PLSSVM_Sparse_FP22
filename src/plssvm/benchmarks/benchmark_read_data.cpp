8aa722d94fd1bc339af8ff1029ace6d26d910253
Tim Schmidt <st163674@stud.uni-stuttgart.de>
2022-07-28 23:21:43 +0200

    Add CSR reading benchmarks

--- src/plssvm/benchmarks/benchmark_read_data.cpp
+++ src/plssvm/benchmarks/benchmark_read_data.cpp
@@ -11,6 +11,7 @@
 #include "plssvm/benchmarks/benchmark_read_data.hpp"
 
 #include <numeric>
+#include <iostream>
 
 namespace plssvm::benchmarks {
 
@@ -21,6 +22,8 @@ void benchmark_read_data::run() {
 
     evaluate_dataset("tiny (~150)", DATASET_TINY);
     evaluate_dataset("small (~5000)", DATASET_SMALL);
+    evaluate_dataset("medium (~50000)", DATASET_MEDIUM);
+    evaluate_dataset("large (~250000)", DATASET_LARGE);
 }
 
 void benchmark_read_data::evaluate_dataset(const std::string sub_benchmark_name, const std::string path_to_dataset) {
@@ -30,9 +33,11 @@ void benchmark_read_data::evaluate_dataset(const std::string sub_benchmark_name,
 
     std::vector<std::vector<real_type>> data_dense;
     plssvm::openmp::coo<real_type> data_coo{};
+    plssvm::openmp::csr<real_type> data_csr{};
 
     auto data_ptr_dense = std::make_shared<const std::vector<std::vector<real_type>>>(std::move(data_dense));
     auto data_ptr_coo = std::make_shared<const plssvm::openmp::coo<real_type>>(std::move(data_coo));
+    auto data_ptr_csr = std::make_shared<const plssvm::openmp::csr<real_type>>(std::move(data_csr));
 
     // dense
     std::vector<std::chrono::nanoseconds> raw_runtimes_dense;
@@ -52,23 +57,37 @@ void benchmark_read_data::evaluate_dataset(const std::string sub_benchmark_name,
         std::chrono::time_point end_time = std::chrono::high_resolution_clock::now();
         raw_runtimes_coo.push_back(std::chrono::round<std::chrono::nanoseconds>(end_time - start_time));
     }
+
+    // csr
+    std::vector<std::chrono::nanoseconds> raw_runtimes_csr;
+    for(size_t i = 0; i < cycles; i++) {
+        std::chrono::time_point start_time = std::chrono::high_resolution_clock::now();
+        params.parse_libsvm_file_sparse_csr(path_to_dataset, data_ptr_csr);
+        std::chrono::time_point end_time = std::chrono::high_resolution_clock::now();
+        raw_runtimes_csr.push_back(std::chrono::round<std::chrono::nanoseconds>(end_time - start_time));
+    }
     
     sub_benchmark_names.push_back(sub_benchmark_name + " dense");
     sub_benchmark_names.push_back(sub_benchmark_name + " COO");
-
+    sub_benchmark_names.push_back(sub_benchmark_name + " CSR");
+    
     // mean
     runtimes_mean.push_back(std::reduce(raw_runtimes_dense.begin(), raw_runtimes_dense.end()) / raw_runtimes_dense.size());
     runtimes_mean.push_back(std::reduce(raw_runtimes_coo.begin(), raw_runtimes_coo.end()) / raw_runtimes_coo.size());
+    runtimes_mean.push_back(std::reduce(raw_runtimes_csr.begin(), raw_runtimes_csr.end()) / raw_runtimes_csr.size());
 
     // median
     std::nth_element(raw_runtimes_dense.begin(), raw_runtimes_dense.begin() + raw_runtimes_dense.size()/2, raw_runtimes_dense.end());
     runtimes_median.push_back(raw_runtimes_dense[raw_runtimes_dense.size()/2]);
     std::nth_element(raw_runtimes_coo.begin(), raw_runtimes_coo.begin() + raw_runtimes_coo.size()/2, raw_runtimes_coo.end());
     runtimes_median.push_back(raw_runtimes_coo[raw_runtimes_coo.size()/2]);
+    std::nth_element(raw_runtimes_csr.begin(), raw_runtimes_csr.begin() + raw_runtimes_csr.size()/2, raw_runtimes_csr.end());
+    runtimes_median.push_back(raw_runtimes_csr[raw_runtimes_csr.size()/2]);
 
     // max
     runtimes_max.push_back(*std::max_element(raw_runtimes_dense.begin(), raw_runtimes_dense.end()));
     runtimes_max.push_back(*std::max_element(raw_runtimes_coo.begin(), raw_runtimes_coo.end()));
+    runtimes_max.push_back(*std::max_element(raw_runtimes_csr.begin(), raw_runtimes_csr.end()));
 }
 
 }  // namespace plssvm::benchmarks
