568f849df203dc2f1d79061815d98b7b5a191b5a
Paul Arlt <st155700@stud.uni-stuttgart.de>
2022-10-05 16:19:02 +0200

    Added caching to coo_svm_kernel
    Former-commit-id: 7426845afd0ef3cb100f4077c62221fdb47a9f76

--- src/plssvm/backends/CUDA/sparse/coo_svm_kernel.cu
+++ src/plssvm/backends/CUDA/sparse/coo_svm_kernel.cu
@@ -15,12 +15,20 @@ namespace plssvm::cuda_coo {
 
 template <typename real_type>
 __global__ void device_kernel_linear(const real_type *q, real_type *ret, const real_type *d, const size_t *col_ids, const size_t *row_ids, const real_type *values, const real_type QA_cost, const real_type cost, const kernel_index_type nnz, const kernel_index_type width, const kernel_index_type height, const real_type add) {
-    kernel_index_type i = (blockIdx.x * blockDim.x + threadIdx.x) * INTERNAL_BLOCK_SIZE;
-    kernel_index_type j = (blockIdx.y * blockDim.y + threadIdx.y) * INTERNAL_BLOCK_SIZE;
+    kernel_index_type i = blockIdx.x * blockDim.x * INTERNAL_BLOCK_SIZE;
+    kernel_index_type j = blockIdx.y * blockDim.y * INTERNAL_BLOCK_SIZE;
 
     if (i < j) {
         return;
     }
+
+    i += threadIdx.x * INTERNAL_BLOCK_SIZE;
+    j += threadIdx.y * INTERNAL_BLOCK_SIZE;
+
+    __shared__ real_type block_c_rows_1[THREAD_BLOCK_SIZE][INTERNAL_BLOCK_SIZE];
+    __shared__ real_type block_c_rows_2[THREAD_BLOCK_SIZE][INTERNAL_BLOCK_SIZE];
+    real_type thread_c_kernel[INTERNAL_BLOCK_SIZE][INTERNAL_BLOCK_SIZE] = { 0.0 };
+    real_type thread_c_row[INTERNAL_BLOCK_SIZE];
 
     kernel_index_type row_1_indices[INTERNAL_BLOCK_SIZE] = { 0 };
     kernel_index_type row_2_indices[INTERNAL_BLOCK_SIZE] = { 0 };
@@ -48,29 +56,55 @@ __global__ void device_kernel_linear(const real_type *q, real_type *ret, const r
         row_2_indices[block_index] = row_2_index;
     }
 
+    for (kernel_index_type feature_index = 0; feature_index < width; ++feature_index) {
+        __syncthreads();
+        #pragma unroll INTERNAL_BLOCK_SIZE
+        for (kernel_index_type internal_index = 0; internal_index < INTERNAL_BLOCK_SIZE; ++internal_index) {
+            const kernel_index_type thread_row_match_1 = internal_index % THREAD_BLOCK_SIZE;
+            if (threadIdx.y == thread_row_match_1) {
+                const kernel_index_type row_1_index = row_1_indices[internal_index];
+                if (col_ids[row_1_index] == feature_index && row_ids[row_1_index] == i) {
+                    block_c_rows_1[threadIdx.x][internal_index] = values[row_1_indices[internal_index]];
+                    row_1_indices[internal_index] = row_1_index + 1;
+                } else {
+                    block_c_rows_1[threadIdx.x][internal_index] = 0;
+                }
+            }
+            const kernel_index_type thread_row_match_2 = (internal_index + INTERNAL_BLOCK_SIZE) % THREAD_BLOCK_SIZE;
+            if (threadIdx.y == thread_row_match_2) {
+                const kernel_index_type row_2_index = row_2_indices[internal_index];
+                if (col_ids[row_2_index] == feature_index) {
+                    block_c_rows_2[threadIdx.x][internal_index] = values[row_2_index];
+                    row_2_indices[internal_index] = row_2_index + 1;
+                } else {
+                    block_c_rows_2[threadIdx.x][internal_index] = 0;
+                }
+            }
+        }
+
+        __syncthreads();
+
+        #pragma unroll INTERNAL_BLOCK_SIZE
+        for (kernel_index_type idx = 0; idx < INTERNAL_BLOCK_SIZE; ++idx) {
+            thread_c_row[idx] = block_c_rows_2[threadIdx.y][idx];
+        }
+
+        #pragma unroll INTERNAL_BLOCK_SIZE
+        for (kernel_index_type y = 0; y < INTERNAL_BLOCK_SIZE; ++y) {
+            const real_type row_1_value = block_c_rows_1[threadIdx.x][y];
+            #pragma unroll INTERNAL_BLOCK_SIZE
+            for (kernel_index_type x = 0; x < INTERNAL_BLOCK_SIZE; ++x) {
+                thread_c_kernel[y][x] += row_1_value * thread_c_row[x];
+            }
+        }
+    }
+
     #pragma unroll INTERNAL_BLOCK_SIZE
     for (kernel_index_type y = 0; y < INTERNAL_BLOCK_SIZE; ++y) {
         real_type ret_jy = 0.0;
         #pragma unroll INTERNAL_BLOCK_SIZE
         for (kernel_index_type x = 0; x < INTERNAL_BLOCK_SIZE; ++x) {
-            kernel_index_type row_1_index = row_1_indices[x];
-            kernel_index_type row_2_index = row_2_indices[y];
-            real_type matr_ix_jy = 0.0;
-
-            // multiply rows
-            while (row_ids[row_1_index] == i + x && row_ids[row_2_index] == j + y) {
-                if (col_ids[row_1_index] == col_ids[row_2_index]) {
-                    matr_ix_jy += values[row_1_index] * values[row_2_index];
-                    row_1_index++;
-                    row_2_index++;
-                } else if (col_ids[row_1_index] < col_ids[row_2_index]) {
-                    row_1_index++;
-                } else {
-                    row_2_index++;
-                }
-            }
-
-            real_type temp = (matr_ix_jy + QA_cost - q[i + x] - q[j + y]) * add;
+            const real_type temp = (thread_c_kernel[x][y] + QA_cost - q[i + y] - q[j + x]) * add;
             if (i + x > j + y) {
                 atomicAdd(&ret[i + x], temp * d[j + y]);
                 ret_jy += temp * d[i + x];
@@ -86,12 +120,20 @@ template __global__ void device_kernel_linear(const double *, double *, const do
 
 template <typename real_type>
 __global__ void device_kernel_poly(const real_type *q, real_type *ret, const real_type *d, const size_t *col_ids, const size_t *row_ids, const real_type *values, const real_type QA_cost, const real_type cost, const kernel_index_type nnz, const kernel_index_type width, const kernel_index_type height, const real_type add, const int degree, const real_type gamma, const real_type coef0) {
-    kernel_index_type i = (blockIdx.x * blockDim.x + threadIdx.x) * INTERNAL_BLOCK_SIZE;
-    kernel_index_type j = (blockIdx.y * blockDim.y + threadIdx.y) * INTERNAL_BLOCK_SIZE;
+    kernel_index_type i = blockIdx.x * blockDim.x * INTERNAL_BLOCK_SIZE;
+    kernel_index_type j = blockIdx.y * blockDim.y * INTERNAL_BLOCK_SIZE;
 
     if (i < j) {
         return;
     }
+
+    i += threadIdx.x * INTERNAL_BLOCK_SIZE;
+    j += threadIdx.y * INTERNAL_BLOCK_SIZE;
+
+    __shared__ real_type block_c_rows_1[THREAD_BLOCK_SIZE][INTERNAL_BLOCK_SIZE];
+    __shared__ real_type block_c_rows_2[THREAD_BLOCK_SIZE][INTERNAL_BLOCK_SIZE];
+    real_type thread_c_kernel[INTERNAL_BLOCK_SIZE][INTERNAL_BLOCK_SIZE] = { 0.0 };
+    real_type thread_c_row[INTERNAL_BLOCK_SIZE];
 
     kernel_index_type row_1_indices[INTERNAL_BLOCK_SIZE] = { 0 };
     kernel_index_type row_2_indices[INTERNAL_BLOCK_SIZE] = { 0 };
@@ -119,29 +161,55 @@ __global__ void device_kernel_poly(const real_type *q, real_type *ret, const rea
         row_2_indices[block_index] = row_2_index;
     }
 
+    for (kernel_index_type feature_index = 0; feature_index < width; ++feature_index) {
+        __syncthreads();
+        #pragma unroll INTERNAL_BLOCK_SIZE
+        for (kernel_index_type internal_index = 0; internal_index < INTERNAL_BLOCK_SIZE; ++internal_index) {
+            const kernel_index_type thread_row_match_1 = internal_index % THREAD_BLOCK_SIZE;
+            if (threadIdx.y == thread_row_match_1) {
+                const kernel_index_type row_1_index = row_1_indices[internal_index];
+                if (col_ids[row_1_index] == feature_index && row_ids[row_1_index] == i) {
+                    block_c_rows_1[threadIdx.x][internal_index] = values[row_1_indices[internal_index]];
+                    row_1_indices[internal_index] = row_1_index + 1;
+                } else {
+                    block_c_rows_1[threadIdx.x][internal_index] = 0;
+                }
+            }
+            const kernel_index_type thread_row_match_2 = (internal_index + INTERNAL_BLOCK_SIZE) % THREAD_BLOCK_SIZE;
+            if (threadIdx.y == thread_row_match_2) {
+                const kernel_index_type row_2_index = row_2_indices[internal_index];
+                if (col_ids[row_2_index] == feature_index) {
+                    block_c_rows_2[threadIdx.x][internal_index] = values[row_2_index];
+                    row_2_indices[internal_index] = row_2_index + 1;
+                } else {
+                    block_c_rows_2[threadIdx.x][internal_index] = 0;
+                }
+            }
+        }
+
+        __syncthreads();
+
+        #pragma unroll INTERNAL_BLOCK_SIZE
+        for (kernel_index_type idx = 0; idx < INTERNAL_BLOCK_SIZE; ++idx) {
+            thread_c_row[idx] = block_c_rows_2[threadIdx.y][idx];
+        }
+
+        #pragma unroll INTERNAL_BLOCK_SIZE
+        for (kernel_index_type y = 0; y < INTERNAL_BLOCK_SIZE; ++y) {
+            const real_type row_1_value = block_c_rows_1[threadIdx.x][y];
+            #pragma unroll INTERNAL_BLOCK_SIZE
+            for (kernel_index_type x = 0; x < INTERNAL_BLOCK_SIZE; ++x) {
+                thread_c_kernel[y][x] += row_1_value * thread_c_row[x];
+            }
+        }
+    }
+
     #pragma unroll INTERNAL_BLOCK_SIZE
     for (kernel_index_type y = 0; y < INTERNAL_BLOCK_SIZE; ++y) {
         real_type ret_jy = 0.0;
         #pragma unroll INTERNAL_BLOCK_SIZE
         for (kernel_index_type x = 0; x < INTERNAL_BLOCK_SIZE; ++x) {
-            kernel_index_type row_1_index = row_1_indices[x];
-            kernel_index_type row_2_index = row_2_indices[y];
-            real_type matr_ix_jy = 0.0;
-
-            // multiply rows
-            while (row_ids[row_1_index] == i + x && row_ids[row_2_index] == j + y) {
-                if (col_ids[row_1_index] == col_ids[row_2_index]) {
-                    matr_ix_jy += values[row_1_index] * values[row_2_index];
-                    row_1_index++;
-                    row_2_index++;
-                } else if (col_ids[row_1_index] < col_ids[row_2_index]) {
-                    row_1_index++;
-                } else {
-                    row_2_index++;
-                }
-            }
-
-            real_type temp = (pow(gamma * matr_ix_jy + coef0, degree) + QA_cost - q[i + x] - q[j + y]) * add;
+            const real_type temp = (pow(gamma * thread_c_kernel[x][y] + coef0, degree) + QA_cost - q[i + y] - q[j + x]) * add;
             if (i + x > j + y) {
                 atomicAdd(&ret[i + x], temp * d[j + y]);
                 ret_jy += temp * d[i + x];
@@ -157,16 +225,25 @@ template __global__ void device_kernel_poly(const double *, double *, const doub
 
 template <typename real_type>
 __global__ void device_kernel_radial(const real_type *q, real_type *ret, const real_type *d, const size_t *col_ids, const size_t *row_ids, const real_type *values, const real_type QA_cost, const real_type cost, const kernel_index_type nnz, const kernel_index_type width, const kernel_index_type height, const real_type add, const real_type gamma) {
-    kernel_index_type i = (blockIdx.x * blockDim.x + threadIdx.x) * INTERNAL_BLOCK_SIZE;
-    kernel_index_type j = (blockIdx.y * blockDim.y + threadIdx.y) * INTERNAL_BLOCK_SIZE;
+    kernel_index_type i = blockIdx.x * blockDim.x * INTERNAL_BLOCK_SIZE;
+    kernel_index_type j = blockIdx.y * blockDim.y * INTERNAL_BLOCK_SIZE;
 
     if (i < j) {
         return;
     }
 
+    i += threadIdx.x * INTERNAL_BLOCK_SIZE;
+    j += threadIdx.y * INTERNAL_BLOCK_SIZE;
+
+    __shared__ real_type block_c_rows_1[THREAD_BLOCK_SIZE][INTERNAL_BLOCK_SIZE];
+    __shared__ real_type block_c_rows_2[THREAD_BLOCK_SIZE][INTERNAL_BLOCK_SIZE];
+    real_type thread_c_kernel[INTERNAL_BLOCK_SIZE][INTERNAL_BLOCK_SIZE] = { 0.0 };
+    real_type thread_c_row[INTERNAL_BLOCK_SIZE];
+
     kernel_index_type row_1_indices[INTERNAL_BLOCK_SIZE] = { 0 };
     kernel_index_type row_2_indices[INTERNAL_BLOCK_SIZE] = { 0 };
 
+    #pragma unroll INTERNAL_BLOCK_SIZE
     for (kernel_index_type block_index = 0; block_index < INTERNAL_BLOCK_SIZE; ++block_index) {
         kernel_index_type row_1_index = nnz * (i + block_index) / height;
         kernel_index_type row_2_index = nnz * (j + block_index) / height;
@@ -189,39 +266,55 @@ __global__ void device_kernel_radial(const real_type *q, real_type *ret, const r
         row_2_indices[block_index] = row_2_index;
     }
 
+    for (kernel_index_type feature_index = 0; feature_index < width; ++feature_index) {
+        __syncthreads();
+        #pragma unroll INTERNAL_BLOCK_SIZE
+        for (kernel_index_type internal_index = 0; internal_index < INTERNAL_BLOCK_SIZE; ++internal_index) {
+            const kernel_index_type thread_row_match_1 = internal_index % THREAD_BLOCK_SIZE;
+            if (threadIdx.y == thread_row_match_1) {
+                const kernel_index_type row_1_index = row_1_indices[internal_index];
+                if (col_ids[row_1_index] == feature_index && row_ids[row_1_index] == i) {
+                    block_c_rows_1[threadIdx.x][internal_index] = values[row_1_indices[internal_index]];
+                    row_1_indices[internal_index] = row_1_index + 1;
+                } else {
+                    block_c_rows_1[threadIdx.x][internal_index] = 0;
+                }
+            }
+            const kernel_index_type thread_row_match_2 = (internal_index + INTERNAL_BLOCK_SIZE) % THREAD_BLOCK_SIZE;
+            if (threadIdx.y == thread_row_match_2) {
+                const kernel_index_type row_2_index = row_2_indices[internal_index];
+                if (col_ids[row_2_index] == feature_index) {
+                    block_c_rows_2[threadIdx.x][internal_index] = values[row_2_index];
+                    row_2_indices[internal_index] = row_2_index + 1;
+                } else {
+                    block_c_rows_2[threadIdx.x][internal_index] = 0;
+                }
+            }
+        }
+
+        __syncthreads();
+
+        #pragma unroll INTERNAL_BLOCK_SIZE
+        for (kernel_index_type idx = 0; idx < INTERNAL_BLOCK_SIZE; ++idx) {
+            thread_c_row[idx] = block_c_rows_2[threadIdx.y][idx];
+        }
+
+        #pragma unroll INTERNAL_BLOCK_SIZE
+        for (kernel_index_type y = 0; y < INTERNAL_BLOCK_SIZE; ++y) {
+            const real_type row_1_value = block_c_rows_1[threadIdx.x][y];
+            #pragma unroll INTERNAL_BLOCK_SIZE
+            for (kernel_index_type x = 0; x < INTERNAL_BLOCK_SIZE; ++x) {
+                thread_c_kernel[y][x] += (row_1_value - thread_c_row[x]) * (row_1_value - thread_c_row[x]);
+            }
+        }
+    }
+
+    #pragma unroll INTERNAL_BLOCK_SIZE
     for (kernel_index_type y = 0; y < INTERNAL_BLOCK_SIZE; ++y) {
         real_type ret_jy = 0.0;
+        #pragma unroll INTERNAL_BLOCK_SIZE
         for (kernel_index_type x = 0; x < INTERNAL_BLOCK_SIZE; ++x) {
-            kernel_index_type row_1_index = row_1_indices[x];
-            kernel_index_type row_2_index = row_2_indices[y];
-            real_type matr_ix_jy = 0.0;
-
-            // calc sq. e. dist
-            while (row_ids[row_1_index] == i + x && row_ids[row_2_index] == j + y) {
-                if (col_ids[row_1_index] == col_ids[row_2_index]) {
-                    matr_ix_jy += (values[row_1_index] - values[row_2_index]) * (values[row_1_index] - values[row_2_index]);
-                    row_1_index++;
-                    row_2_index++;
-                } else if (col_ids[row_1_index] < col_ids[row_2_index]) {
-                    matr_ix_jy += values[row_1_index] * values[row_1_index];
-                    row_1_index++;
-                } else {
-                    matr_ix_jy += values[row_2_index] * values[row_2_index];
-                    row_2_index++;
-                }
-            }
-            if (row_ids[row_1_index] != i + x) {
-                for (; row_2_index < nnz && row_ids[row_2_index] == j + y; ++row_2_index) {
-                    matr_ix_jy += values[row_2_index] * values[row_2_index];
-                }
-            }
-            if (row_ids[row_2_index] != j + y) {
-                for (; row_1_index < nnz && row_ids[row_1_index] == i + x; ++row_1_index) {
-                    matr_ix_jy += values[row_1_index] * values[row_1_index];
-                }
-            }
-
-            real_type temp = (exp(-gamma * matr_ix_jy) + QA_cost - q[i + x] - q[j + y]) * add;
+            const real_type temp = (exp(-gamma * thread_c_kernel[x][y]) + QA_cost - q[i + y] - q[j + x]) * add;
             if (i + x > j + y) {
                 atomicAdd(&ret[i + x], temp * d[j + y]);
                 ret_jy += temp * d[i + x];
@@ -235,4 +328,4 @@ __global__ void device_kernel_radial(const real_type *q, real_type *ret, const r
 template __global__ void device_kernel_radial(const float *, float *, const float *, const size_t *, const size_t *, const float *, const float, const float, const kernel_index_type, const kernel_index_type, const kernel_index_type, const float, const float);
 template __global__ void device_kernel_radial(const double *, double *, const double *, const size_t *, const size_t *, const double *, const double, const double, const kernel_index_type, const kernel_index_type, const kernel_index_type, const double, const double);
 
-}  // namespace plssvm::cuda
+}  // namespace plssvm::cuda_coo
